6097ad267866cb7e9cddcc6428749255e9e9f547
mahi1212 <mahi121.mr@gmail.com>
2021-08-18 22:54:23 +0600

    update product working

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
-function updateCaseNumber(isIncreasing){
-    const caseInput = document.getElementById('case-number')
+function updateCaseNumber(product, isIncreasing){
+    const caseInput = document.getElementById(product + '-number')
     const caseNumber = caseInput.value
-    const casePrice = document.getElementById('case-price')
+    const casePrice = document.getElementById(product + '-price')
     const casePriceText = casePrice.innerText
-
 
     if(isIncreasing == true){
         caseInput.value = parseInt(caseNumber) + 1
-        casePrice.innerText = parseFloat(casePriceText) + 59 ;
+        if((product + '-price') == 'case-price'){
+            casePrice.innerText = parseFloat(casePriceText) + 59 ;
+        }else{
+            casePrice.innerText = parseFloat(casePriceText) + 1219 ;
+        }
     }
     else if(caseNumber > 0){
         caseInput.value = parseInt(caseNumber) - 1
-        casePrice.innerText = parseFloat(casePriceText) - 59 ;
+        if((product + '-price') == 'case-price'){
+            casePrice.innerText = parseFloat(casePriceText) - 59 ;
+        }else{
+            casePrice.innerText = parseFloat(casePriceText) - 1219 ;
+        }
     }
-
-    
 }
 
-document.getElementById('case-plus').addEventListener('click', function(){
-    updateCaseNumber(true)
-    // const caseInput = document.getElementById('case-number')
-    // const caseNumber = caseInput.value
-    // caseInput.value = parseInt(caseNumber) + 1
-    // const casePrice = document.getElementById('case-price')
-    // const casePriceValue = casePrice.innerText
-    // casePrice.innerText = parseFloat(casePriceText) + 59 ;
+// phone function
+document.getElementById('phone-minus').addEventListener('click', function(){
+    updateCaseNumber('phone' , false)
+})
+document.getElementById('phone-plus').addEventListener('click', function(){
+    updateCaseNumber('phone', true)
+})
 
+// Casing function
+document.getElementById('case-plus').addEventListener('click', function(){
+    updateCaseNumber('case', true)
 })
 
 document.getElementById('case-minus').addEventListener('click', function(){
-    updateCaseNumber(false)
+    updateCaseNumber('case',false)
 })
 
 
