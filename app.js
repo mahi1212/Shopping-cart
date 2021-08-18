064646ce9518adda43638eb6a049431899c0b909
mahi1212 <mahi121.mr@gmail.com>
2021-08-18 22:30:43 +0600

    casing adding done

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('I am in a separate file')
+function updateCaseNumber(isIncreasing){
+    const caseInput = document.getElementById('case-number')
+    const caseNumber = caseInput.value
+    const casePrice = document.getElementById('case-price')
+    const casePriceText = casePrice.innerText
+
+
+    if(isIncreasing == true){
+        caseInput.value = parseInt(caseNumber) + 1
+        casePrice.innerText = parseFloat(casePriceText) + 59 ;
+    }
+    else if(caseNumber > 0){
+        caseInput.value = parseInt(caseNumber) - 1
+        casePrice.innerText = parseFloat(casePriceText) - 59 ;
+    }
+
+    
+}
+
+document.getElementById('case-plus').addEventListener('click', function(){
+    updateCaseNumber(true)
+    // const caseInput = document.getElementById('case-number')
+    // const caseNumber = caseInput.value
+    // caseInput.value = parseInt(caseNumber) + 1
+    // const casePrice = document.getElementById('case-price')
+    // const casePriceValue = casePrice.innerText
+    // casePrice.innerText = parseFloat(casePriceText) + 59 ;
+
+})
+
+document.getElementById('case-minus').addEventListener('click', function(){
+    updateCaseNumber(false)
+})
+
